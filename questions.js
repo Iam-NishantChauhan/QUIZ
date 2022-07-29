ae196ba9aa2476abb194e8e022afb6c1d0d4c4f0
Nishant Chauhan <90475952+Iam-NishantChauhan@users.noreply.github.com>
2022-07-29 10:21:28 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Who was recently appointed as the third advisor to the Lieutenant Governor of the Union Territory of Jammu and Kashmir?",
+    answer: "Rajiv Rai Bhatnagar",
+    options: [
+      "Vijay Kumar",
+      "Rajiv Rai Bhatnagar",
+      "G C Murmu",
+      "Sailendra Babu"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The book- ‘A Child of Destiny’, which was recently released by the Indian Vice President, is an autobiography of which Indian educationist?",
+    answer: "K. Ramakrishna Rao",
+    options: [
+      "Girishwar Misra",
+      "Amit Abraham",
+      "Devendra Singh",
+      "K. Ramakrishna Rao"
+    ]
+  },
+    {
+    numb: 3,
+    question: "As per the recent NSO Update (June 2022), what is the GDP growth estimate of India in 2021-22?",
+    answer: "8.7 per cent",
+    options: [
+      "8.7 per cent",
+      "8.9 per cent",
+      "8.5 per cent",
+      "9.2 per cent"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which state has decided to conduct a caste-based census named ‘Jaati Aadharit Ganana’?",
+    answer: "Bihar",
+    options: [
+      "Uttar Pradesh",
+      "Madhya Pradesh",
+      "Jharkhand",
+      "Bihar"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which institution developed India’s first ‘Farmland price index’?",
+    answer: "IIM Ahmadabad",
+    options: [
+      "IIM Ahmadabad",
+      "IIT Madras",
+      "IIM Bengaluru",
+      "IISc Bengaluru"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
